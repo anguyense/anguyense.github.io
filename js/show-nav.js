d64915df4ca44f2857726d52b7acef73568c84a7
Anh N <anguyen.se@gmail.com>
2022-09-13 00:15:39 -0400

    A while ago
    Never committed this until now.
    Removed comments
    Fixed social links
    Fixed code structure

---
+++ js/show-nav.js
@@ -0,0 +1,11 @@
+function toggleNav() {
+    var hiddenNav = document.getElementById("nav-list")
+    if (hiddenNav.style.display === "block") 
+    {
+        hiddenNav.style.display = "none";
+    }
+    else 
+    {
+        hiddenNav.style.display = "block";
+    }
+}
