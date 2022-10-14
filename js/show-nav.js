5932f23f2f67a932cb03112de5ac6428a6c975ab
Anh Nguyen <anguyen.se@gmail.com>
2022-10-14 10:26:14 -0400

    Responsive Navbar
    Horizontal navbar for smaller viewport functional
    TODO: Make navbar collaspible vertically for better UX

--- js/show-nav.js
+++ js/show-nav.js
@@ -3,9 +3,11 @@ function toggleNav() {
     if (hiddenNav.style.display === "block") 
     {
         hiddenNav.style.display = "none";
+        // Decrease height of nav to hide links
     }
     else 
     {
         hiddenNav.style.display = "block";
+        // Increase height of nav to show all links
     }
 }
